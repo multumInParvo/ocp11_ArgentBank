9d94d6a128b3d4c1017de9e58939ef8cc4f49765
Oleksandr Pryvalov <oleksandrpryv@gmail.com>
2024-04-08 17:35:37 +0200

    mapped item in homepage, features section

--- argent-bank-app/src/pages/Home.js
+++ argent-bank-app/src/pages/Home.js
@@ -1,11 +1,37 @@
 // Home.js
-import React from 'react';
-import Banner from '../components/Banner';
-import '../styles/Home.css';
 
-function Home () {
+import React from 'react'
+import Banner from '../components/Banner'
+import '../styles/Home.css'
+import FeaturesData from '../data/FeaturesData.json'
+import FeatureItem from '../components/FeatureItem'
+import imgChat from '../img/icon-chat.png'
+import imgMoney from '../img/icon-money.png'
+import imgSecurity from '../img/icon-security.png'
+
+function Home() {
+
+    const IconImage = {
+        "icon-chat.png": imgChat,
+        "icon-money.png": imgMoney,
+        "icon-security.png": imgSecurity
+    }
+
     return (
-        <Banner />
+        <main>
+            <Banner />
+            <section className='features'>
+                {FeaturesData.map((data) => (
+                <FeatureItem
+                    key={data.id}
+                    image={IconImage[data.image]}
+                    imageAlt={data.imageAlt}
+                    title={data.title}
+                    description={data.description}
+                />
+            ))}
+            </section>
+        </main>
     )
 }
 
